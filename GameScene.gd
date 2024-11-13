b317fec73e26bd1b0e80d83fe489d6c577db4acc
Xphost <xphost@foxmail.com>
2024-11-14 02:22:39 +0800

    fix some bug and add some new function

--- GameScene.gd
+++ GameScene.gd
@@ -1,25 +1,28 @@
 extends Node2D
 
-var chess
 # 值~每个雷代表-1，周围的数字代表周围雷数量。0~8
-var b_chess
+var chess
+var chess_lock
 var scene_button
 var scene_label
 var time
 var is_lock = false
 var mine = 0
+var flag = 0
 
 func _ready() -> void:
 	chess = initialize_2d_array(Global.height, Global.width, 0)
-	b_chess = initialize_2d_array(Global.height, Global.width, 0)
+	chess_lock = initialize_2d_array(Global.height, Global.width, true)
 	scene_button = initialize_2d_array(Global.height, Global.width, null)
 	scene_label = initialize_2d_array(Global.height, Global.width, null)
 	time = Global.second
+	flag = Global.mine
 	$LabelSecond.text = "剩余秒数：" + str(time)
+	$LabelFlag.text = "剩余旗子：" + str(flag)
 	# 埋雷
 	for i in range(Global.mine):
 		_rand_mine()
-	# 将b_chess赋值
+	# 将chess赋值
 	_reload_array()
 	# 布置场景+蒙布
 	_reload_scene()
@@ -49,7 +52,7 @@ func _rand_mine():
 		var i = randi() % Global.height
 		var j = randi() % Global.width
 		if(chess[i][j] == 0):
-			chess[i][j] = 1
+			chess[i][j] = -1
 			break
 	
 # 初始化一次二维数组。
@@ -67,34 +70,34 @@ func _reload_array():
 	for i in range(Global.height):
 		for j in range(Global.width):
 			var foo = 0;
-			if chess[i][j] == 1:
+			if chess[i][j] == -1:
 				foo = -1
 			else:
 				if i > 0 and j > 0:
-					if chess[i - 1][j - 1] == 1:
+					if chess[i - 1][j - 1] == -1:
 						foo += 1
 				if i > 0 and j < Global.width - 1:
-					if chess[i - 1][j + 1] == 1:
+					if chess[i - 1][j + 1] == -1:
 						foo += 1
 				if i < Global.height - 1 and j > 0:
-					if chess[i + 1][j - 1] == 1:
+					if chess[i + 1][j - 1] == -1:
 						foo += 1
 				if i < Global.height - 1 and j < Global.width - 1:
-					if chess[i + 1][j + 1] == 1:
+					if chess[i + 1][j + 1] == -1:
 						foo += 1
 				if i > 0:
-					if chess[i - 1][j] == 1:
+					if chess[i - 1][j] == -1:
 						foo += 1
 				if j > 0:
-					if chess[i][j - 1] == 1:
+					if chess[i][j - 1] == -1:
 						foo += 1
 				if i < Global.height - 1:
-					if chess[i + 1][j] == 1:
+					if chess[i + 1][j] == -1:
 						foo += 1
 				if j < Global.width - 1:
-					if chess[i][j + 1] == 1:
+					if chess[i][j + 1] == -1:
 						foo += 1
-			b_chess[i][j] = foo
+			chess[i][j] = foo
 			
 # 将color的int数字，转换成float形式的数据并返回。
 func color_int_to_float(r: int, g: int, b: int) -> Color:
@@ -104,8 +107,36 @@ func _reload_scene():
 	for i in Global.height:
 		for j in Global.width:
 			scene_label[i][j] = Label.new()
+			var label = StyleBoxFlat.new()
+			label.border_width_top = 2
+			label.border_width_right = 2
+			label.border_width_left = 2
+			label.border_width_bottom = 2
+			label.border_color = color_int_to_float(10, 10, 10)
+			scene_label[i][j].add_theme_stylebox_override("normal", label)
 			scene_label[i][j].add_theme_font_size_override("font_size", 29)
-			scene_label[i][j].text = str(b_chess[i][j]) if b_chess[i][j] != -1 else "" if b_chess[i][j] == 0 else "雷"
+			scene_label[i][j].text = str(chess[i][j]) if chess[i][j] > 0 else "" if chess[i][j] == 0 else "雷"
+			match chess[i][j]:
+				-1:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(30, 30, 120))
+				1:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(0, 128, 255))
+				2:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(0, 128, 0))
+				3:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(192, 0, 0))
+				4:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(0, 0, 96))
+				5:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(0, 0, 96))
+				6:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(0, 96, 96))
+				7:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(0, 0, 0))
+				8:
+					scene_label[i][j].add_theme_color_override("font_color", color_int_to_float(160, 160, 160))
+				_:
+					pass
 			scene_label[i][j].horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 			scene_label[i][j].vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 			scene_label[i][j].size.x = 40
@@ -115,11 +146,26 @@ func _reload_scene():
 			$MineScenePanel.add_child(scene_label[i][j])
 			scene_button[i][j] = Button.new()
 			var normal = StyleBoxFlat.new()
-			normal.bg_color = color_int_to_float(20, 50, 70)
+			normal.border_width_top = 2
+			normal.border_width_right = 2
+			normal.border_width_left = 2
+			normal.border_width_bottom = 2
+			normal.border_color = color_int_to_float(10, 10, 10)
+			normal.bg_color = color_int_to_float(60, 60, 60)
 			var hover = StyleBoxFlat.new()
-			hover.bg_color = color_int_to_float(80, 0, 20)
+			hover.border_width_top = 2
+			hover.border_width_right = 2
+			hover.border_width_left = 2
+			hover.border_width_bottom = 2
+			hover.border_color = color_int_to_float(10, 10, 10)
+			hover.bg_color = color_int_to_float(40, 40, 40)
 			var pressed = StyleBoxFlat.new()
-			pressed.bg_color = color_int_to_float(30, 50, 30)
+			pressed.border_width_top = 2
+			pressed.border_width_right = 2
+			pressed.border_width_left = 2
+			pressed.border_width_bottom = 2
+			pressed.border_color = color_int_to_float(10, 10, 10)
+			pressed.bg_color = color_int_to_float(20, 20, 20)
 			scene_button[i][j].add_theme_stylebox_override("pressed", pressed)
 			scene_button[i][j].add_theme_stylebox_override("normal", normal)
 			scene_button[i][j].add_theme_stylebox_override("hover", hover)
@@ -127,18 +173,59 @@ func _reload_scene():
 			scene_button[i][j].size.y = 40
 			scene_button[i][j].position.x = j * 40
 			scene_button[i][j].position.y = i * 40
-			scene_button[i][j].pressed.connect(_on_any_button_pressed.bind(i, j))
+			#scene_button[i][j].horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+			#scene_button[i][j].vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+			scene_button[i][j].add_theme_font_size_override("font_size", 29)
+			scene_button[i][j].connect("gui_input", Callable(self, "_on_any_button_gui_input").bind(i, j))
 			$MineScenePanel.add_child(scene_button[i][j])
 			
-func _on_any_button_pressed(i: int, j: int):
-	if not is_lock:
+func _on_any_button_gui_input(event, i: int, j: int):
+	if event is InputEventMouseButton and event.pressed:
+		# 左键，执行on_any_button_pressed
+		if event.button_index == 1:
+			_on_any_button_step(i, j)
+		# 右键，为按钮标旗子~
+		elif event.button_index == 2:
+			_on_any_button_flag(i, j)
+func _on_any_button_flag(i: int, j: int):
+	if flag > 0:
+		if chess_lock[i][j]:
+			scene_button[i][j].text = "旗"
+			flag -= 1
+		else:
+			scene_button[i][j].text = ""
+			flag += 1
+		chess_lock[i][j] = not chess_lock[i][j]
+		$LabelFlag.text = "剩余旗子：" + str(flag)
+	else:
+		if not chess_lock[i][j]:
+			scene_button[i][j].text = ""
+			flag += 1
+			chess_lock[i][j] = not chess_lock[i][j]
+			$LabelFlag.text = "剩余旗子：" + str(flag)
+func _on_any_button_step(i: int, j: int):
+	if not is_lock and chess_lock[i][j]:
 		if scene_button[i][j] != null:
 			remove_child(scene_button[i][j])
 			scene_button[i][j].queue_free()
 			scene_button[i][j] = null;
-			if chess[i][j] == 1:
+			if chess[i][j] == -1:
 				is_lock = true
 				$LabelWin.text = "你输了~"
+				for k in range(Global.height):
+					for l in range(Global.width):
+						if scene_button[k][l] != null and chess[k][l] == -1:
+							remove_child(scene_button[k][l])
+							scene_button[k][l].queue_free()
+							scene_button[k][l] = null;
+				var normal = StyleBoxFlat.new()
+				normal.border_width_top = 2
+				normal.border_width_right = 2
+				normal.border_width_left = 2
+				normal.border_width_bottom = 2
+				normal.border_color = color_int_to_float(10, 10, 10)
+				normal.bg_color = color_int_to_float(200, 0, 0)
+				scene_label[i][j].add_theme_stylebox_override("normal", normal)
 				return
 			mine += 1
 			Global.score += 10
@@ -146,23 +233,23 @@ func _on_any_button_pressed(i: int, j: int):
 			if mine >= Global.width * Global.height - Global.mine:
 				is_lock = true
 				$LabelWin.text = "你赢了~"
-			if b_chess[i][j] == 0:
+			if chess[i][j] == 0:
 				if i > 0 and j > 0:
-					_on_any_button_pressed(i - 1, j - 1)
+					_on_any_button_step(i - 1, j - 1)
 				if i > 0 and j < Global.width - 1:
-					_on_any_button_pressed(i - 1, j + 1)
+					_on_any_button_step(i - 1, j + 1)
 				if i < Global.height - 1 and j > 0:
-					_on_any_button_pressed(i + 1, j - 1)
+					_on_any_button_step(i + 1, j - 1)
 				if i < Global.height - 1 and j < Global.width - 1:
-					_on_any_button_pressed(i + 1, j + 1)
+					_on_any_button_step(i + 1, j + 1)
 				if i > 0:
-					_on_any_button_pressed(i - 1, j)
+					_on_any_button_step(i - 1, j)
 				if j > 0:
-					_on_any_button_pressed(i, j - 1)
+					_on_any_button_step(i, j - 1)
 				if i < Global.height - 1:
-					_on_any_button_pressed(i + 1, j)
+					_on_any_button_step(i + 1, j)
 				if j < Global.width - 1:
-					_on_any_button_pressed(i, j + 1)
+					_on_any_button_step(i, j + 1)
 		
 			
 func _on_button_return_pressed() -> void:
